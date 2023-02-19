6bc54f9b182d7579389e9700cbac1dd14c4c0f87
ldctp <luciannadaisy@gmail.com>
2023-02-19 19:29:25 +0000

    Updated design on rock paper scissors game

--- RockPaperScissors/rockpaperscissors.js
+++ RockPaperScissors/rockpaperscissors.js
@@ -1,9 +1,47 @@
-document.querySelector('#submit').addEventListener('click', runGame);
-
+let playerChoice = "";
+let computerChoice = "";
+let winner = "";
 let computerScore = 0;
 let playerScore = 0;
 
-function responseAssign(){
+
+document.querySelector('#rock').addEventListener('click', assignRock);
+document.querySelector('#rock').addEventListener('click', runGame);
+document.querySelector('#paper').addEventListener('click', assignPaper);
+document.querySelector('#paper').addEventListener('click', runGame);
+document.querySelector('#scissors').addEventListener('click', assignScissors);
+document.querySelector('#scissors').addEventListener('click', runGame);
+document.querySelector('#playagain').addEventListener('click', resestGame);
+
+
+function resestGame() {
+    playerChoice = "";
+    computerChoice = "";
+    winner = "";
+    computerScore = 0;
+    playerScore = 0;
+    document.querySelector('#playagain').innerText = "";
+    document.querySelector('#playagain').classList.remove("playagain");
+    document.querySelector('#computerChoice').innerText = "";
+    document.querySelector('#result').innerText = "";
+    document.querySelector('#playerScore').innerText = `Player score: ${playerScore}`;
+    document.querySelector('#computerScore').innerText = `Computer score: ${computerScore}`;
+}
+
+
+function assignRock(){
+    return playerChoice = 'Rock';
+}
+
+function assignPaper(){
+    return playerChoice = 'Paper';
+}
+
+function assignScissors(){
+    return playerChoice = 'Scissors';
+}
+
+function computerResponse(){
     const response = Math.floor(Math.random() * 3) + 1;
     if (response == 1){
        return 'Rock'; 
@@ -16,50 +54,58 @@ function responseAssign(){
     }
 };
 
-function compareAnswers(computerGuess, playerGuess){
-    if (playerGuess === computerGuess){
-        return 'It\'s a draw!';
-    }
-    else if (playerGuess == 'Rock' && computerGuess == 'Paper'){
-        return 'Computer Wins!';
-    }
-    else if (playerGuess == 'Rock' && computerGuess == 'Scissors'){
-        return 'You Win!';
-    }
-    else if (playerGuess == 'Paper' && computerGuess == 'Rock'){
-        return 'You Win!';
-    }
-    else if (playerGuess == 'Paper' && computerGuess == 'Scissors'){
-        return 'Computer Wins!';
-    }
-    else if (playerGuess == 'Scissors' && computerGuess == 'Rock'){
-        return 'Computer Wins!';
-    }
-    else if (playerGuess == 'Scissors' && computerGuess == 'Paper'){
-        return 'You Win!';
+function emojiAssign(computerChoice){
+    if (computerChoice == 'Rock'){
+        return '🪨'; 
+     }
+     else if (computerChoice == 'Paper'){
+         return '📃';
+     }
+     else if (computerChoice == 'Scissors'){
+         return '✂️';
+     }
 }
-};
 
-function scoreCount(winner){
-    if (winner == "Computer Wins!"){
+function scoreAnswers(computerChoice){
+    if (playerChoice == 'Rock' && computerChoice == 'Paper' || playerChoice == 'Paper' && computerChoice == 'Scissors' || playerChoice == 'Scissors' && computerChoice == 'Rock'){
         return computerScore+=1
     }
-    else if (winner == "You Win!"){
+    else if (playerChoice == 'Rock' && computerChoice == 'Scissors' || playerChoice == 'Paper' && computerChoice == 'Rock' || playerChoice == 'Scissors' && computerChoice == 'Paper'){
         return playerScore+=1;
     }
-}
-
-
-function runGame(){
-    const playerGuess = document.querySelector('input[name="option"]:checked').value;
-    document.querySelector('#playerGuess').innerText = playerGuess;
-    let computerGuess = responseAssign();
-    document.querySelector('#computerGuess').innerText = computerGuess;
-    const winner = compareAnswers(computerGuess, playerGuess);
-    document.querySelector('#result').innerText = winner;
-    const scoring = scoreCount(winner);
-    document.querySelector('#playerScore').innerText = "Player score: " + playerScore;
-    document.querySelector('#computerScore').innerText = "Computer score: " + computerScore;
 };
 
+function scoreReturn(computerChoice){
+    if (computerScore == 5){
+        document.querySelector('#playagain').innerText = "Play Again?";
+        document.querySelector('#playagain').classList.add("playagain");
+        return "Game over: The Computer won!"
+    }
+    else if (playerScore == 5){
+        document.querySelector('#playagain').innerText = "Play Again?";
+        document.querySelector('#playagain').classList.add("playagain");
+        return "Game over: You won!"
+    }
+    else if (playerChoice == 'Rock' && computerChoice == 'Paper' || playerChoice == 'Paper' && computerChoice == 'Scissors' || playerChoice == 'Scissors' && computerChoice == 'Rock'){
+        return "1 point to Computer"
+    }
+    else if (playerChoice == 'Rock' && computerChoice == 'Scissors' || playerChoice == 'Paper' && computerChoice == 'Rock' || playerChoice == 'Scissors' && computerChoice == 'Paper'){
+        return "1 point to you"
+    }
+    else if (playerChoice == computerChoice){
+        return "Draw"
+    }
+};
 
+function runGame(){
+    if (playerScore < 5 && computerScore < 5){
+        computerChoice = computerResponse();
+        let computerEmoji = emojiAssign(computerChoice);
+        document.querySelector('#computerChoice').innerText = computerEmoji;
+        scoreAnswers(computerChoice, playerChoice);
+        let result = scoreReturn(computerChoice)
+        document.querySelector('#result').innerText = result;
+        document.querySelector('#playerScore').innerText = `Player score: ${playerScore}`;
+        document.querySelector('#computerScore').innerText = `Computer score: ${computerScore}`;
+    }
+};
